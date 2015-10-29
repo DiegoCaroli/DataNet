b44faf46a8242518ef806935dd3c322735755264
Diego <diegocaroli@icloud.com>
2015-10-29 17:39:41 +0100

    Add NetActivity.

---
+++ app/src/main/java/com/android/diego/datanet/Net.java
@@ -0,0 +1,22 @@
+package com.android.diego.datanet;
+
+/**
+ * Created by Diego on 29/10/2015.
+ */
+public class Net {
+
+    private String mName;
+
+    public Net() {
+        mName = "";
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+}
--- app/src/main/java/com/android/diego/datanet/NetActivity.java
+++ app/src/main/java/com/android/diego/datanet/NetActivity.java
@@ -2,12 +2,57 @@ package com.android.diego.datanet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NetActivity extends AppCompatActivity {
+
+    private Net mNet;
+    private EditText mNetField;
+    private Button mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net);
+
+        mNet = new Net();
+
+        mNetField = (EditText) findViewById(R.id.edit_net);
+        mNetField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mNet.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_insert_net);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkNameNet();
+            }
+        });
+    }
+
+    private void checkNameNet() {
+        if (mNet.getName() == "") {
+            Toast.makeText(this, R.string.empty_name_net, Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
