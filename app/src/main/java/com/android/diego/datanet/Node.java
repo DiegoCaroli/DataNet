0863e516453b67cdc245a503e642927dae59cdde
Diego <diegocaroli@icloud.com>
2015-10-29 17:46:08 +0100

    Add Displaying List

---
+++ app/src/main/java/com/android/diego/datanet/Node.java
@@ -0,0 +1,15 @@
+package com.android.diego.datanet;
+
+/**
+ * Created by Diego on 29/10/2015.
+ */
+public class Node {
+
+    private String mName;
+    private String mOutcomes;
+    private String mParents;
+    private String mProbabilities;
+
+    public Node() {
+    }
+}
