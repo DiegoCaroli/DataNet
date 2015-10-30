a67baddb8c442e19cc7b52feb9e57669283c5653
Diego <diegocaroli@icloud.com>
2015-10-30 15:09:03 +0100

    Add Displaying List. Creating new controllers for recycle view

--- app/src/main/java/com/android/diego/datanet/NodeListActivityFragment.java
+++ app/src/main/java/com/android/diego/datanet/NodeListActivityFragment.java
@@ -2,14 +2,23 @@ package com.android.diego.datanet;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class NodeListActivityFragment extends Fragment {
+
+    private RecyclerView mNodeRecyclerView;
+    private NodeAdapter mAdapter;
 
     public NodeListActivityFragment() {
     }
@@ -17,6 +26,79 @@ public class NodeListActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_node_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_node_list, container, false);
+
+        mNodeRecyclerView = (RecyclerView) view
+                .findViewById(R.id.node_recycler_view);
+        mNodeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
+        return view;
+    }
+
+    private void updateUI() {
+        NodeStore nodeStore = NodeStore.get(getActivity());
+        List<Node> nodes = nodeStore.getNodes();
+
+        mAdapter = new NodeAdapter(nodes);
+        mNodeRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class NodeHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        private Node mNode;
+
+        public NodeHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_node_title_text_view);
+            //mDateTextView = (TextView) itemView.findViewById(R.id.list_item_node_date_text_view);
+        }
+
+        public void bindNode(Node node) {
+            mNode = node;
+            mTitleTextView.setText(mNode.getName());
+            //mDateTextView.setText(mNode.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mNode.getName() + " clicked!", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private class NodeAdapter extends RecyclerView.Adapter<NodeHolder> {
+
+        private List<Node> mNodes;
+
+        public NodeAdapter(List<Node> nodes) {
+            mNodes = nodes;
+        }
+
+        @Override
+        public NodeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_node, parent, false);
+            return new NodeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(NodeHolder holder, int position) {
+            Node node = mNodes.get(position);
+            holder.bindNode(node);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNodes.size();
+        }
     }
 }
