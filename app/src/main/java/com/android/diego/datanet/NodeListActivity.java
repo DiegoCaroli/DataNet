ffaca14925bb26da3285b7d530012355e5eefe8f
Diego <diegocaroli@icloud.com>
2015-10-30 17:40:18 +0100

    add toolbar

--- app/src/main/java/com/android/diego/datanet/NodeListActivity.java
+++ app/src/main/java/com/android/diego/datanet/NodeListActivity.java
@@ -3,13 +3,9 @@ package com.android.diego.datanet;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 public class NodeListActivity extends AppCompatActivity {
 
@@ -20,37 +16,7 @@ public class NodeListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(true);
-        //actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_node_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_insert_node) {
-            Toast.makeText(getApplicationContext(), "Insert Node Pressed",
-                    Toast.LENGTH_LONG).show();
-        } else if (id == R.id.action_insert_node_with_parents) {
-            Toast.makeText(getApplicationContext(), "Insert Node Parents Pressed",
-                    Toast.LENGTH_LONG).show();
-        } else if (id == R.id.action_done) {
-            Toast.makeText(getApplicationContext(), "Done Pressed",
-                    Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
