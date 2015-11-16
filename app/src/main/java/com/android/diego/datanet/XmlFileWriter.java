f8adac66595300173bed9198264da7c7562bed65
Diego <diegocaroli@icloud.com>
2015-11-16 16:56:55 +0100

    Add XmlFileWriter.

---
+++ app/src/main/java/com/android/diego/datanet/XmlFileWriter.java
@@ -0,0 +1,13 @@
+package com.android.diego.datanet;
+
+import java.io.File;
+
+/**
+ * Created by Diego on 16/11/2015.
+ */
+public class XmlFileWriter {
+
+    public static void writeXmlFile(File fileName, NodeStore nodeStore) {
+
+    }
+}
