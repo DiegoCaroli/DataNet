3ae64ad5c3441c108bd79a85f69a91e01f0b60b2
Diego <diegocaroli@icloud.com>
2015-12-06 22:39:04 +0100

    Update URLServer.

--- app/src/main/java/com/android/diego/datanet/Model/URLServer.java
+++ app/src/main/java/com/android/diego/datanet/Model/URLServer.java
@@ -10,7 +10,7 @@ public class URLServer {
     private String mURL;
 
     private URLServer(){
-        mURL = "http://localhost:8080/BayesService/upload/";
+        mURL = "http://10.0.2.2:8080/BayesService";
     }
 
     public static URLServer getInstance() {
